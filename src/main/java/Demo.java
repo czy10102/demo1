8dfe1cf7e5e1d83abc0d500b383d142d0f2d71ce
雷神 <6318272@qq.com>
2020-06-30 07:59:44 +0800

    first commit

---
+++ src/main/java/Demo.java
@@ -0,0 +1,21 @@
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Demo {
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        // 读取配置文件
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        // 构建sqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        User user = sqlSession.selectOne("MyMapper.selectUser");
+        System.out.println(user);
+    }
+}
